497be84ff7444570475b9fa43727e11438b0e640
Paul <ramoneblack@me.com>
2024-05-06 08:38:44 -0400

    baseMameUrlsUpdate

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ mongoose.connect(urlDB)
 const axios = require('axios')
 const Redis = require('redis')
 const drinksApi = process.env.DRINK_PUBLIC_KEY
+const allCocktailsAPi = process.env.ALL_COCKTAILS_API_KEY
+const mustKnowApi = process.env.DRINK_MUST_KNOWS_KEY
+const allShotsApi = process.env.ALL_SHOTS_API_KEY
 const redisClient = Redis.createClient({legacyMode: true}) // in production const client = Redis.createClient({ url })
 const DEFAULT_EXPIRATION = 3600
 
@@ -66,21 +69,59 @@ app.post("/saveDrinkandDates", async (req, res) => {
 
 app.get("/drinks", async (req, res) => {
     const drinkId = req.query.drinkId
-   
+    
+
     redisClient.get("drinks", async (error, drinks) => {
         if (error) {
-            console.log('cache hit')
             console.error(error)
             await redisClient.connect()
         }
         if (drinks != null){
-            console.log('cache hit')
             return res.json(JSON.parse(drinks))
            
         } else {
-            console.log('cache not hit')
-            const { data: response } = await axios.get(drinksApi, {params: { drinkId }})
+            const { data: response } = await axios.get(drinksApi, {params: { drinkId }}) 
             redisClient.SETEX('drinks', DEFAULT_EXPIRATION, JSON.stringify(response)) 
+            res.json(response)
+        }
+    })
+
+})
+
+app.get("/cocktails", async (req, res) => {
+    const drinkId = req.query.drinkId
+    
+
+    redisClient.get("cocktails", async (error, cocktails) => {
+        if (error) {
+            console.error(error)
+            await redisClient.connect()
+        }
+        if (cocktails != null){
+            return res.json(JSON.parse(cocktails))
+           
+        } else {
+            const { data: response } = await axios.get(allCocktailsAPi, {params: { drinkId }}) 
+            redisClient.SETEX('cocktails', DEFAULT_EXPIRATION, JSON.stringify(response)) 
+            res.json(response)
+        }
+    })
+
+})
+
+app.get("/must-knows", async (req, res) => {
+    const drinkId = req.query.drinkId
+    redisClient.get("must-knows", async (error, mustKnows) => {
+        if (error) {
+            console.error(error)
+            await redisClient.connect()
+        }
+        if (mustKnows != null){
+            return res.json(JSON.parse(mustKnows))
+           
+        } else {
+            const { data: response } = await axios.get(mustKnowApi, {params: { drinkId }}) 
+            redisClient.SETEX('must-knows', DEFAULT_EXPIRATION, JSON.stringify(response)) 
             res.json(response)
         }
         
@@ -88,6 +129,29 @@ app.get("/drinks", async (req, res) => {
     })
 
 })
+
+
+app.get("/shot", async (req, res) => {
+    const drinkId = req.query.drinkId
+    redisClient.get("shot", async (error, shots) => {
+        if (error) {
+            console.error(error)
+            await redisClient.connect()
+        }
+        if (shots != null){
+            return res.json(JSON.parse(shots))
+           
+        } else {
+            const { data: response } = await axios.get(allShotsApi, {params: { drinkId }}) 
+            redisClient.SETEX('shot', DEFAULT_EXPIRATION, JSON.stringify(response)) 
+            res.json(response)
+        }
+        
+
+    })
+
+})
+
 
   
 app.listen(3001, () => {
